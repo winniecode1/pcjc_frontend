1c3320acadf63d1f28283c628e7cb551ab3c4e88
akamyc <mycc_1@163.com>
2025-11-26 17:50:23 +0800

    tj

--- src/pages/AttributionDiagnosisV2.vue
+++ src/pages/AttributionDiagnosisV2.vue
@@ -4,7 +4,7 @@
     <b-row class="header-bar align-items-center no-gutters">
       <b-col cols="3" class="text-left">
         <button class="header-btn btn-home" @click="$router.push('/')">首页</button>
-        <button class="header-btn btn-back" @click="$router.back()">上个界面</button>
+        <button class="header-btn btn-back" @click="$router.back()">上个页面</button>
       </b-col>
     </b-row>
 
