6417df26543ead2d347ee225cbf801c62ea1c87d
akamyc <mycc_1@163.com>
2026-04-26 11:04:31 +0800

    tj

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  VUE_APP_STAGE1_API_BASE_URL: '"http://10.109.253.71:5237"'
+  VUE_APP_STAGE1_API_BASE_URL: '"http://10.109.253.71:5237"',
+  VUE_APP_MODULE5_API_BASE_URL: '"http://10.109.253.71:5235"'
 }
